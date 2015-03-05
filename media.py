8d1f172526fe8762108dd9d5c6be1fb53763df74
Manuel Alire <manuelalire@msn.com>
2015-03-05 14:25:11 -0800

    Update media.py

--- media.py
+++ media.py
@@ -16,11 +16,12 @@ class Movie():
     def __init__(self,movie_title, movie_storyline, box_office, poster_image, trailer_youtube):
         """
         Movie Class constructor, creates space in memory and assigns arguments to accessible varibles.
-
-        movie_storyline - string describing storyline of movie
-        box_office - int of of global box office receipts
-        poster_image - url of box office movie poster
-        trailer_youtube - url of youtube movie trailer
+        
+        arguments:
+            movie_storyline - string describing storyline of movie
+            box_office - int of of global box office receipts
+            poster_image - url of box office movie poster
+            trailer_youtube - url of youtube movie trailer
         """
         self.title = movie_title
         self.storyline = movie_storyline
